8f43c9b4da53e60fc167acdec7859f9eb598f254
nico britz <nico.britz@outlook.com>
2022-11-17 15:41:25 +0000

    add favicon and tidy descrition

--- src/Layouts/Create.js
+++ src/Layouts/Create.js
@@ -31,7 +31,7 @@ const Create = ({ currentUser, updatePosts }) => {
       >
         <ProfileImage userProfile={currentUser} />
         <textarea
-          className="w-3/4 h-40 p-3 border rounded-md border-cyan-400 dark:text-black"
+          className="w-3/4 h-20 p-3 border rounded-md border-cyan-400 dark:text-black"
           name="content"
           value={formContent}
           onChange={(e) => setFormContent(e.target.value)}
